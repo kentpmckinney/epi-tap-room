412eaea3c4c2aafabbe45a0cdfc282db4a0e13b9
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-08 21:27:12 -0700

    add tests for ADD, UPDATE, DELETE actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
 
 const reducer = (state = [], action) => {
-  const { type, payload } = action;
+  const { type, key, brand, pintPrice, alcoholContent, pintsRemaining, isGlutenFree, isVegan } = action;
   switch (type) {
     case 'ADD_ITEM':
-      return [{ ...payload }, ...state];
+      return [{ key, brand, pintPrice, alcoholContent, pintsRemaining, isGlutenFree, isVegan }, ...state];
+    case 'UPDATE_ITEM':
+      return state.map(item => item.key === key ? { key, brand, pintPrice, alcoholContent, pintsRemaining, isGlutenFree, isVegan } : item);
+    case 'DELETE_ITEM':
+      return state.filter(item => item.key !== key);
     default:
       return state;
   }
 }
 
-export default combineReducers({ reducer: reducer });
+export default reducer;
