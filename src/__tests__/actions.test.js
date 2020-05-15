2c9517a6fd805e1b1df7a7665535225a85f04be6
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-14 18:54:52 -0700

    test passes for the updateItem action creator

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -2,10 +2,16 @@ import { addItem, updateItem, deleteItem, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } f
 
 describe('action creator tests', () => {
 
-  test('actionAddItem', () => {
+  test('addItem', () => {
     const action = addItem(1, 'test', 'test', 0.00, 0.0, 124, false, false, 1);
     expect(action).toEqual({ type: ADD_ITEM, data: { name: 'test', brand: 'test', pricePerPint: 0.00, alcoholContent: 0.0, pintsRemaining: 124, isGlutenFree: false, isVegan: false, key: 1 } });
   });
+
+  test('updateItem', () => {
+    const action = updateItem(1, 'test', 'test', 0.00, 0.0, 124, false, false, 1);
+    expect(action).toEqual({ type: UPDATE_ITEM, data: { name: 'test', brand: 'test', pricePerPint: 0.00, alcoholContent: 0.0, pintsRemaining: 124, isGlutenFree: false, isVegan: false, key: 1 } });
+  });
+
 
 
 
