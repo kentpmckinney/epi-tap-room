93239c10a7e55c6e8e54532634b4bfe3aacf74f6
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-14 20:43:42 -0700

    add action type constants to reducer code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
+import { ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from '../reducers'
+
 const reducer = (state = [], action) => {
   const { type, data } = action;
   switch (type) {
-    case 'ADD_ITEM':
+    case ADD_ITEM:
       return [{ ...data }, ...state];
-    case 'UPDATE_ITEM':
+    case UPDATE_ITEM:
       return state.map(item => item.key === data.key ? { ...data } : item);
-    case 'DELETE_ITEM':
+    case DELETE_ITEM:
       return state.filter(item => item.key !== data.key);
     default:
       return state;
