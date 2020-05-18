4a9b708b2001cdfb631568755fa72a7d49aaf32c
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-18 11:03:49 -0700

    Updates

---
+++ precache-manifest.5df924ef96da8899ed56bd4ab9ff4e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce84f3ed0d5a3e6903ac35d98327f4f",
+    "url": "/epi-tap-room-redux/index.html"
+  },
+  {
+    "revision": "7c836eec3b7936633ad8",
+    "url": "/epi-tap-room-redux/static/css/main.e4525080.chunk.css"
+  },
+  {
+    "revision": "7760296ced1471045d16",
+    "url": "/epi-tap-room-redux/static/js/2.ce1a462a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/epi-tap-room-redux/static/js/2.ce1a462a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c836eec3b7936633ad8",
+    "url": "/epi-tap-room-redux/static/js/main.a8a3ab67.chunk.js"
+  },
+  {
+    "revision": "e3f1b1fb09fd7bd11def",
+    "url": "/epi-tap-room-redux/static/js/runtime-main.2ea6c258.js"
+  }
+]);
